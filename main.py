7ad30d2616ea03b096403f9ae0441bce6b6d16aa
santho91 <santosanto240391@gmail.com>
2026-03-30 19:32:44 +0530

    Fix indentation and update template directory

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from fastapi import FastAPI, Request, Form
 from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
-templates = Jinja2Templates(directory="/code")
+templates = Jinja2Templates(directory="templates")
 
 @app.get("/")
 def form_post(request: Request):
